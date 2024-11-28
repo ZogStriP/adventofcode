433409e1b7da81a7cdceea53e4c07357838ecc83
zogstrip <regis@hanol.fr>
2024-11-29 00:21:54 +0100

    ruby.version is shorter

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,5 @@
-{ pkgs, nixpkgs-ruby, ... }:
 {
   env.RUBY_YJIT_ENABLE = true;
-
   languages.ruby.enable = true;
-  languages.ruby.package = nixpkgs-ruby.packages.${pkgs.system}."ruby-3.3";
+  languages.ruby.version = "3.3";
 }

8c3b9bf737feaff26838ae14387fe5a3d7b29261
zogstrip <regis@hanol.fr>
2024-12-02 09:58:02 +0100

    no parentheses needed here

--- 2024/02.rb
+++ 2024/02.rb
@@ -1,4 +1,4 @@
-l = $<.map { _1.split.map(&:to_i) }
+l = $<.map { _1.split.map &:to_i }
 def diff(l) = l.each_cons(2).map { _1 - _2 }
 def safe(l) = diff(l).all? { (1..3) === _1 } || diff(l).all? { (-3..-1) === _1 }
 p l.count { safe _1 }

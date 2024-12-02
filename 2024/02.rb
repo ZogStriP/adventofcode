d69d998c7ca76f46c6c2401ad9de064fa9a6023d
zogstrip <regis@hanol.fr>
2024-12-02 20:16:01 +0100

    using combination to make day 2 even shorter

--- 2024/02.rb
+++ 2024/02.rb
@@ -2,4 +2,4 @@ l = $<.map { _1.split.map &:to_i }
 def diff(l) = l.each_cons(2).map { _1 - _2 }
 def safe(l) = diff(l).all? { (1..3) === _1 } || diff(l).all? { (-3..-1) === _1 }
 p l.count { safe _1 }
-p l.count { |l| l.size.times.any? { safe l[..._1] + l[_1 + 1..] } }
+p l.count { |l| l.combination(l.size - 1).any? { safe _1 } }

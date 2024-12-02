c1e84859b86d6403bf51cfc5a964f13d6ac53e1f
zogstrip <regis@hanol.fr>
2024-12-02 09:54:40 +0100

    2024 day 2

---
+++ 2024/02.rb
@@ -0,0 +1,5 @@
+l = $<.map { _1.split.map(&:to_i) }
+def diff(l) = l.each_cons(2).map { _1 - _2 }
+def safe(l) = diff(l).all? { (1..3) === _1 } || diff(l).all? { (-3..-1) === _1 }
+p l.count { safe _1 }
+p l.count { |l| l.size.times.any? { safe l[..._1] + l[_1 + 1..] } }

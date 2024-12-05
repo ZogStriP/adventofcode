ec92d497526020c32231c878202d963895db288f
zogstrip <regis@hanol.fr>
2024-12-05 12:42:20 +0100

    slightly shorter day 5

--- 2024/05.rb
+++ 2024/05.rb
@@ -5,7 +5,7 @@ rules.map { a, b = _1.split ?|; (R[a] ||= []) << b }
 
 y, n = pages
   .map { _1.split ?, }
-  .partition { |p| (0...p.size).all? { (p[_1 + 1..] & R[p[_1]]).size == p[_1 + 1..].size } }
+  .partition { |p| (0...p.size).all? { (p[_1 + 1..] & R[p[_1]]).size == p.size - _1 - 1 } }
 
 n.map! { _1.sort { |a, b| R[a].include?(b) ? -1 : R[b].include?(a) ? 1 : 0 } }
 
